7bde36e9f702dbe1d3fcc39e46fe325451f30ce0
Taras Drapalyuk <taras@drapalyuk.com>
2015-05-31 17:17:08 +0300

    upd tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from gorinich.base import *
+import unittest
+
+from gorinich.base import TemplateRenderingMixin, BaseHandler
 from gorinich.core import *
 from gorinich.handlers import *
 from gorinich.objects import *
 from gorinich.run import *
 from gorinich.utils import *
+
+class TestInit(unittest.TestCase):
+    def test_init(self):
+        self.assertIsInstance(TemplateRenderingMixin(), TemplateRenderingMixin)
