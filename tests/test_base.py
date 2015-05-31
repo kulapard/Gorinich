c7b90d1d9b3ee84a062b3208af3e6c285cbf5ee5
Taras Drapalyuk <taras@drapalyuk.com>
2015-05-31 13:18:03 +0300

    add tests

---
+++ tests/test_base.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from gorinich.base import *
+from gorinich.core import *
+from gorinich.handlers import *
+from gorinich.objects import *
+from gorinich.run import *
+from gorinich.utils import *
